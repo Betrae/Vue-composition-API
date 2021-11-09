0de6c89f669d75c05d131235b648ba111098f633
Reynaldo de Guzman <rdeguzman.itso@tip.edu.ph>
2021-11-09 14:20:25 +0800

    Finish Project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from '@vue/reactivity';
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
 
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
-    const ninjaOne = ref({ name: 'mario', age: 30})
-    const ninjaTwo = reactive({ name: 'luigi', age: 35})
+    const { posts, error, load } = getPosts()
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
+    load()
 
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
     return {
-      ninjaOne,
-      updateNinjaOne,
-      ninjaTwo,
-      updateNinjaTwo
+        posts,
+        error
     }
   }
 }
